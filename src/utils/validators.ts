e5259fb675b40245dd6d63f1336abb4073a8c3b1
bayunvnt <bayunoviantoo9@gmail.com>
2022-08-02 21:11:46 +0700

    fix: isExists validator

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -16,9 +16,12 @@ export class IsExist implements ValidatorConstraintInterface {
   async validate(value: string, validationArguments: ValidationArguments) {
     const repository = validationArguments.constraints[0];
     const pathToProperty = validationArguments.constraints[1];
-    const entity: unknown = await getRepository(repository).findOne({
-      [pathToProperty ? pathToProperty : validationArguments.property]:
-        pathToProperty ? value?.[pathToProperty] : value,
+    const entity = await getRepository(repository).findOne({
+      [pathToProperty ? pathToProperty : validationArguments.property]: value?.[
+        pathToProperty
+      ]
+        ? value?.[pathToProperty]
+        : value,
     });
 
     return Boolean(entity);

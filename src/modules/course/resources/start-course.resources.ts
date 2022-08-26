bdbe1acedbf44c0fc6f26cfd8f3c941f2be64d79
bayunvnt <bayunoviantoo9@gmail.com>
2022-08-26 16:53:01 +0700

    feat: add provider name onstart mooc

--- src/modules/course/resources/start-course.resources.ts
+++ src/modules/course/resources/start-course.resources.ts
@@ -3,6 +3,7 @@ import { Course } from 'src/entities/course.entity';
 export const StartCourseResource = (course: Course, type: number): any => {
   return {
     status: type,
+    provider_name: course.provider.name,
     url: course && course.url ? course.url : null,
   };
 };

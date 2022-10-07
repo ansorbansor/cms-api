35eea04ef4b5dfedd9a090cfc4719001fe079e50
bayunvnt <bayunoviantoo9@gmail.com>
2022-10-07 17:38:34 +0700

    fix(course-fetch): change credential do dengansenangteknologi.com

--- src/modules/course-fetch/course-fetch.service.ts
+++ src/modules/course-fetch/course-fetch.service.ts
@@ -126,7 +126,7 @@ export class CourseFetchService {
       &fields[course]=title,url,image_480x270,context_info,visible_instructors,locale,estimated_content_length,rating,num_reviews,description,objectives_summary,content_info_short,instructional_level_simple,price_detail`;
       const headers = {
         Authorization:
-          'Basic MXlsc3RPU1YyMXpHcWJ2cG9wOW1EN243RTFnT3RnTEdkMlJ3a1FsTzpqTGF6QzVHS0lTM3c5dGRDdm00MXRuamlxUjZhTjE0blloOTA4b1hlZzNJaFNWSlNxbVFrS2Zxc0lwQlN4bW1xUEdqODZDVHJKWEtlVEZPWFg5ckV6SVlWaEEzaDZCektjM0s5QkJMR3ZPR1RDcU9uMWQ0TFY0WFpudnhxYTVBVw==',
+          'Basic RmVVcUl2UWI5QkxJZmZXN1ZtOVZORWVvZmtXWHNQWmpZVjU4VU9VcTo2Ym1ROTJpRnJpQUtRdkRYRWI0SUt0cGhMcU0wZGlHOHFSQVlNdHYxcE4yOU1BaUdDY1R3akNzQ2hQb0RFUkkxWkdlWTVJM2s1UEN6VThBTXRFeThhM0pVUmVCRXZmUnZ0eUtrZjllbnMxZTJsUmJacUdOeGdFZ0drcE94NGNsNw==',
         Accept: 'application/json, text/plain, */*',
         'Content-Type': 'application/json;charset=utf-8',
       };

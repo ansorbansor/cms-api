4c3c0c722fd56c804107f546f8112f0ed98778c9
bayunvnt <bayunoviantoo9@gmail.com>
2022-10-18 18:39:23 +0700

    feat(course): create empty param message

--- src/modules/course/dto/create-course.dto.ts
+++ src/modules/course/dto/create-course.dto.ts
@@ -22,49 +22,49 @@ export class CreateCourseDto {
   external_id: string;
 
   @ApiProperty({ example: 'Pelatih A' })
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Pelatih tidak boleh kosong' })
   coach?: string;
 
   @ApiProperty({ example: 1, description: 'in minute' })
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Durasi tidak boleh kosong' })
   @Transform(({ value }) => value * 40)
   duration: number;
 
   @ApiProperty({ example: 1 })
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Penyelenggara tidak boleh kosong' })
   @Validate(IsExist, ['Provider', 'id'], {
     message: 'Penyelenggara tidak tersedia',
   })
   provider_id: number;
 
   @ApiProperty({ example: 1 })
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Kategori tidak boleh kosong' })
   @Validate(IsExist, ['CourseCategory', 'id'], {
     message: 'Kategori tidak tersedia',
   })
   category_id: number;
 
   @ApiProperty({ example: true })
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Status tidak boleh kosong' })
   @Transform(({ value }) => (value === 'true' ? 1 : 0))
   status: number;
 
   @ApiProperty({ example: 1 })
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Topik tidak boleh kosong' })
   @Validate(IsExist, ['Topic', 'id'], {
     message: 'Topik tidak tersedia',
   })
   topic_id: number;
 
   @ApiProperty({ example: 1 })
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Level tidak boleh kosong' })
   @Validate(IsExist, ['CourseLevel', 'id'], {
     message: 'Level tidak tersedia',
   })
   level_id: number;
 
   @ApiProperty({ example: 1 })
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Bahasa tidak boleh kosong' })
   @IsArray()
   @Validate(IsExist, ['CourseLanguage', 'id'], {
     message: 'Bahasa tidak tersedia',
@@ -76,25 +76,25 @@ export class CreateCourseDto {
   date_course?: Date;
 
   @ApiProperty({ example: 1 })
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Rating tidak boleh kosong' })
   @IsEnum(Rating, {
     message: 'Rating hanya 1-5',
   })
   rating: number;
 
   @ApiProperty({ example: 'Lorem Ipsum' })
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Deskripsi tidak boleh kosong' })
   description?: string;
 
   @ApiProperty({
     example:
       'https://www.udemy.com/course/the-ultimate-hands-on-hadoop-tame-your-big-data/',
   })
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'URL tidak boleh kosong' })
   url?: string;
 
   @ApiProperty({ example: 1 })
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'Jenis harga tidak boleh kosong' })
   @Validate(IsExist, ['CoursePrice', 'id'], {
     message: 'Jenis harga tidak tersedia',
   })

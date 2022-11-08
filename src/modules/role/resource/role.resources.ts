29cd7f3f9f44ea19fc353892c19bfeea75a4895c
bayunvnt <bayunoviantoo9@gmail.com>
2022-11-08 19:03:25 +0700

    feat(role): skip read access response if can delete only

--- src/modules/role/resource/role.resources.ts
+++ src/modules/role/resource/role.resources.ts
@@ -1,17 +1,34 @@
 import { Role } from 'src/entities/role.entity';
 
 export const RoleResource = (role: Role): any => {
-  const mapMenu =
-    role.roleAccess != null
-      ? role.roleAccess.map((roleAccess) => {
-          return {
-            role_access_id: roleAccess.id ? roleAccess.id : null,
-            menu_id: roleAccess.menu ? roleAccess.menu.id : null,
-            menu_name: roleAccess.menu ? roleAccess.menu.name : null,
-            menu_access: roleAccess.menu ? roleAccess.menu_access : null,
-          };
+  const mapMenu = [];
+  if (role.roleAccess) {
+    role.roleAccess.forEach((roleAccess) => {
+      //filtering data access, dont return READ access (1) if access only can delete
+      const filterMenu = role.roleAccess.filter((e) => {
+        return e.menu_id == roleAccess.menu_id;
+      });
+      if (
+        roleAccess.menu_access == 1 &&
+        filterMenu.length == 2 &&
+        filterMenu.find((e) => {
+          return e.menu_access == 1;
+        }) &&
+        filterMenu.find((e) => {
+          return e.menu_access == 3;
         })
-      : [];
+      ) {
+        return;
+      }
+
+      mapMenu.push({
+        role_access_id: roleAccess.id ? roleAccess.id : null,
+        menu_id: roleAccess.menu ? roleAccess.menu.id : null,
+        menu_name: roleAccess.menu ? roleAccess.menu.name : null,
+        menu_access: roleAccess.menu ? roleAccess.menu_access : null,
+      });
+    });
+  }
 
   return {
     id: role.id,

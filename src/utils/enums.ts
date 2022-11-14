3e0a6f0356a187e8c652d7be3891e2357f735efa
bayunvnt <bayunoviantoo9@gmail.com>
2022-11-14 19:02:35 +0700

    fix(copytext): auth error message

--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -138,7 +138,7 @@ export enum FilePath {
 
 export enum ErrorMessage {
   NIP_NOT_EXISTS = 'Maaf, NIP yang anda masukkan tidak sesuai. Silahkan coba masukkan kembali NIP yang telah tervalidasi.',
-  EMAIL_NOT_EXISTS = 'Maaf, email yang anda masukkan tidak sesuai. Silahkan coba masukkan kembali email yang telah tervalidasi.',
+  EMAIL_NOT_EXISTS = 'Maaf, Akun yang anda pilih tidak sesuai. Silahkan coba pilih kembali Akun yang telah tervalidasi.',
   PASSWORD_WRONG = 'Maaf, password yang anda masukkan tidak sesuai. Silahkan coba masukkan kembali password anda.',
   USER_NOT_FOUND = 'User tidak ditemukan. Silahkan coba kembali menggunakan NIP yang telah terdaftar.',
   FORBIDDEN = 'User tidak memiliki hak untuk mengakses halaman ini.',

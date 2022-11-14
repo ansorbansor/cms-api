71dbd700d981f8a87ff88da6fa02ac630e64c36c
bayunvnt <bayunoviantoo9@gmail.com>
2022-11-14 15:11:25 +0700

    feat(notification): add google-firebase-dev.json

--- src/modules/user-notification/firebase.module.ts
+++ src/modules/user-notification/firebase.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { readFileSync } from 'fs';
 import * as admin from 'firebase-admin';
+import appConfig from 'src/config/app.config';
 
 @Module({})
 export class FirebaseModule {
   constructor() {
-    const path = __dirname + '/../../utils/google-firebase.json';
+    const path =
+      __dirname +
+      `/../../utils/${
+        appConfig().nodeEnv == 'development'
+          ? 'google-firebase-dev.json'
+          : 'google-firebase.json'
+      }`;
     const data = readFileSync(path, 'utf8');
     const firebaseCredentials = JSON.parse(data);
     admin.initializeApp({

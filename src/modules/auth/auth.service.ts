bddb7308b461c3e404fdb1f3703e3a3de1929340
bayunvnt <bayunoviantoo9@gmail.com>
2022-11-15 16:00:53 +0700

    fix(auth): reverse method auth, check simsdm first then ldap

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -91,139 +91,156 @@ export class AuthService {
     let user = await this.usersService.findOneFull({ nip: loginDto.nip });
 
     if (!user && !onlyAdmin && authConfig().activateLDAP == 'true') {
-      //check Active Directory user
-      const ADResult = await new ActiveDirectoryUtils().authAD(
-        `${loginDto.nip}@setneg.go.id`,
-        loginDto.password,
-      );
+      let simsdmData = await fetch(`${simsdmConfig().url}${loginDto.nip}`);
 
-      if (!ADResult) {
-        throw failedResponse(
-          HttpStatus.UNPROCESSABLE_ENTITY,
-          `${ErrorMessage.NIP_NOT_EXISTS}`,
+      simsdmData = await simsdmData.json();
+
+      if (Array.isArray(simsdmData)) {
+        simsdmData = simsdmData[0];
+      }
+
+      const adHelper = new ActiveDirectoryUtils();
+
+      if (simsdmData) {
+        //check Active Directory user with nip baru
+        let ADResult = await new ActiveDirectoryUtils().authAD(
+          `${adHelper.decryptSIMSDMData(simsdmData.nipbaru)}@setneg.go.id`,
+          loginDto.password,
         );
-      } else {
-        try {
-          let simsdmData = await fetch(`${simsdmConfig().url}${loginDto.nip}`);
 
-          simsdmData = await simsdmData.json();
-
-          if (Array.isArray(simsdmData) && simsdmData.length > 0) {
-            simsdmData = simsdmData[0];
-          }
-
-          const adHelper = new ActiveDirectoryUtils();
-
-          //set dto register for new user
-          const dto = new AuthRegisterLoginDto();
-          dto.email = adHelper.decryptSIMSDMData(simsdmData.email)
-            ? adHelper.decryptSIMSDMData(simsdmData.email)
-            : adHelper.decryptSIMSDMData(simsdmData.email_dinas)
-            ? adHelper.decryptSIMSDMData(simsdmData.email_dinas)
-            : loginDto.nip;
-          dto.name = adHelper.decryptSIMSDMData(simsdmData.nmpeg)
-            ? adHelper.decryptSIMSDMData(simsdmData.nmpeg)
-            : `User ${loginDto.nip}`;
-          dto.nip = adHelper.decryptSIMSDMData(simsdmData.nipbaru)
-            ? adHelper.decryptSIMSDMData(simsdmData.nipbaru)
-            : adHelper.decryptSIMSDMData(simsdmData.niplama)
-            ? adHelper.decryptSIMSDMData(simsdmData.niplama)
-            : adHelper.decryptSIMSDMData(simsdmData.pns_niplama)
-            ? adHelper.decryptSIMSDMData(simsdmData.pns_niplama)
-            : loginDto.nip;
-          dto.password = loginDto.password;
-
-          //find existing position
-          let position = await this.employeePositionRepository.findOne({
-            where: {
-              name: adHelper.decryptSIMSDMData(simsdmData.jabatanakhir),
-            },
-          });
-
-          //create position if not exists
-          if (!position) {
-            position = await this.employeePositionRepository.save({
-              name: adHelper.decryptSIMSDMData(simsdmData.jabatanakhir),
-            });
-          }
-
-          dto.position_id = position.id;
-          dto.provider = AuthProvidersEnum.ldap;
-          dto.role_id = RoleEnum.user; //hardcoded user
-          dto.status = 1;
-
-          //find existing level
-          let level = await this.employeeLevelRepository.findOne({
-            where: {
-              name: adHelper.decryptSIMSDMData(simsdmData.pangkatakhir),
-            },
-          });
-
-          //create level if not exists
-          if (!level) {
-            level = await this.employeeLevelRepository.save({
-              name: adHelper.decryptSIMSDMData(simsdmData.pangkatakhir),
-            });
-          }
-
-          dto.level_id = level.id;
-
-          //find existing unit
-          let unit = await this.employeeUnitRepository.findOne({
-            where: {
-              name: adHelper.decryptSIMSDMData(simsdmData.satorg),
-            },
-          });
-
-          //create unit if not exists
-          if (!unit) {
-            unit = await this.employeeUnitRepository.save({
-              name: adHelper.decryptSIMSDMData(simsdmData.satorg),
-            });
-          }
-          dto.unit_id = unit.id;
-
-          dto.level = 0;
-          dto.photo = null;
-
-          user = await this.register(null, dto, ip);
-
-          if (adHelper.decryptSIMSDMData(simsdmData.foto)) {
-            try {
-              const fileExt = getFileExtension(
-                adHelper.decryptSIMSDMData(simsdmData.foto),
-              );
-              const res = await fetch(
-                `${simsdmConfig().imageUrl}/${adHelper.decryptSIMSDMData(
-                  simsdmData.foto,
-                )}`,
-              );
-
-              const resBuffer = await res.buffer();
-
-              //save get image
-              const photo = await this.fileService.uploadWithMinioBuffer(
-                resBuffer,
-                user.id,
-                `${user.nip}.${fileExt}`,
-                FilePath.USER,
-                'User Photo',
-              );
-
-              await this.userRepository.update(user.id, {
-                photo: photo.id,
-              });
-            } catch (err) {
-              console.log(err);
-            }
-          }
-        } catch (e) {
-          console.log(e);
-          throw failedResponse(
-            HttpStatus.UNPROCESSABLE_ENTITY,
-            ErrorMessage.USER_NOT_FOUND,
+        //check Active Directory user with nip lama if nip baru not authenticate
+        if (!ADResult) {
+          ADResult = await new ActiveDirectoryUtils().authAD(
+            `${adHelper.decryptSIMSDMData(simsdmData.niplama)}@setneg.go.id`,
+            loginDto.password,
           );
         }
+
+        if (!ADResult) {
+          throw failedResponse(
+            HttpStatus.UNPROCESSABLE_ENTITY,
+            `${ErrorMessage.NIP_NOT_EXISTS}`,
+          );
+        } else {
+          try {
+            //set dto register for new user
+            const dto = new AuthRegisterLoginDto();
+            dto.email = adHelper.decryptSIMSDMData(simsdmData.email)
+              ? adHelper.decryptSIMSDMData(simsdmData.email)
+              : adHelper.decryptSIMSDMData(simsdmData.email_dinas)
+              ? adHelper.decryptSIMSDMData(simsdmData.email_dinas)
+              : loginDto.nip;
+            dto.name = adHelper.decryptSIMSDMData(simsdmData.nmpeg)
+              ? adHelper.decryptSIMSDMData(simsdmData.nmpeg)
+              : `User ${loginDto.nip}`;
+            dto.nip = adHelper.decryptSIMSDMData(simsdmData.nipbaru)
+              ? adHelper.decryptSIMSDMData(simsdmData.nipbaru)
+              : adHelper.decryptSIMSDMData(simsdmData.niplama)
+              ? adHelper.decryptSIMSDMData(simsdmData.niplama)
+              : adHelper.decryptSIMSDMData(simsdmData.pns_niplama)
+              ? adHelper.decryptSIMSDMData(simsdmData.pns_niplama)
+              : loginDto.nip;
+
+            dto.password = loginDto.password;
+            console.log(dto.password);
+
+            //find existing position
+            let position = await this.employeePositionRepository.findOne({
+              where: {
+                name: adHelper.decryptSIMSDMData(simsdmData.jabatanakhir),
+              },
+            });
+
+            //create position if not exists
+            if (!position) {
+              position = await this.employeePositionRepository.save({
+                name: adHelper.decryptSIMSDMData(simsdmData.jabatanakhir),
+              });
+            }
+
+            dto.position_id = position.id;
+            dto.provider = AuthProvidersEnum.ldap;
+            dto.role_id = RoleEnum.user; //hardcoded user
+            dto.status = 1;
+
+            //find existing level
+            let level = await this.employeeLevelRepository.findOne({
+              where: {
+                name: adHelper.decryptSIMSDMData(simsdmData.pangkatakhir),
+              },
+            });
+
+            //create level if not exists
+            if (!level) {
+              level = await this.employeeLevelRepository.save({
+                name: adHelper.decryptSIMSDMData(simsdmData.pangkatakhir),
+              });
+            }
+
+            dto.level_id = level.id;
+
+            //find existing unit
+            let unit = await this.employeeUnitRepository.findOne({
+              where: {
+                name: adHelper.decryptSIMSDMData(simsdmData.satorg),
+              },
+            });
+
+            //create unit if not exists
+            if (!unit) {
+              unit = await this.employeeUnitRepository.save({
+                name: adHelper.decryptSIMSDMData(simsdmData.satorg),
+              });
+            }
+            dto.unit_id = unit.id;
+
+            dto.level = 0;
+            dto.photo = null;
+
+            user = await this.register(null, dto, ip);
+
+            if (adHelper.decryptSIMSDMData(simsdmData.foto)) {
+              try {
+                const fileExt = getFileExtension(
+                  adHelper.decryptSIMSDMData(simsdmData.foto),
+                );
+                const res = await fetch(
+                  `${simsdmConfig().imageUrl}/${adHelper.decryptSIMSDMData(
+                    simsdmData.foto,
+                  )}`,
+                );
+
+                const resBuffer = await res.buffer();
+
+                //save get image
+                const photo = await this.fileService.uploadWithMinioBuffer(
+                  resBuffer,
+                  user.id,
+                  `${user.nip}.${fileExt}`,
+                  FilePath.USER,
+                  'User Photo',
+                );
+
+                await this.userRepository.update(user.id, {
+                  photo: photo.id,
+                });
+              } catch (err) {
+                console.log(err);
+              }
+            }
+          } catch (e) {
+            console.log(e);
+            throw failedResponse(
+              HttpStatus.UNPROCESSABLE_ENTITY,
+              ErrorMessage.USER_NOT_FOUND,
+            );
+          }
+        }
+      } else {
+        throw failedResponse(
+          HttpStatus.UNPROCESSABLE_ENTITY,
+          ErrorMessage.USER_NOT_FOUND,
+        );
       }
     }
 

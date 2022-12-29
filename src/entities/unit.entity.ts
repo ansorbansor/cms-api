38cc840e665a56eed4f84e741f2229d61a3e569b
bayunvnt <bayunoviantoo9@gmail.com>
2022-12-29 12:48:09 +0700

    chore: change unit from int to string

--- src/entities/unit.entity.ts
+++
@@ -1,8 +0,0 @@
-import { Column, Entity } from 'typeorm';
-import { EntityHelper } from 'src/utils/entity-helper';
-
-@Entity({ name: 'units' })
-export class Unit extends EntityHelper {
-  @Column()
-  name?: string;
-}

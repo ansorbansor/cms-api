a2a4c044544e4ba10bbb697dc80f10266e78e54c
bayunvnt <bayunoviantoo9@gmail.com>
2023-01-22 13:13:50 +0700

    feat: add photo on login response

--- src/modules/auth/resources/auth.resources.ts
+++ src/modules/auth/resources/auth.resources.ts
@@ -1,3 +1,4 @@
+import minioConfig from 'src/config/minio.config';
 import { User } from 'src/entities/user.entity';
 
 export const AuthResource = (token: string, user: User): any => {
@@ -14,6 +15,10 @@ export const AuthResource = (token: string, user: User): any => {
       id: user.id,
       name: user.name,
       email: user.email,
+      photo:
+        user.photoFile && user.photoFile.path
+          ? minioConfig().fullUrl + user.photoFile.path
+          : null,
       provider: user.provider,
       status: user.status,
       notification_token: user.notification_token,

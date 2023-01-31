5b81dbc4ea743bce5f0584b170c84a9f3aec8f05
bayunvnt <bayunoviantoo9@gmail.com>
2023-01-31 20:06:45 +0700

    fix(po): resource detail date format

--- src/modules/purchase-orders/resources/purchase-order.resources.ts
+++ src/modules/purchase-orders/resources/purchase-order.resources.ts
@@ -74,9 +74,15 @@ export const PurchaseOrderDetailResource = (po: PurchaseOrder): any => {
       id: po.bidding_area ? po.bidding_area.id : null,
       name: po.bidding_area ? po.bidding_area.name : '-',
     },
-    publish_date: po.publish_date ? po.publish_date : '-',
-    start_date: po.start_date ? po.start_date : '-',
-    end_date: po.end_date ? po.end_date : '-',
+    publish_date: po.publish_date
+      ? moment(po.publish_date).format('yyyy-MM-D HH:mm:ss')
+      : null,
+    start_date: po.start_date
+      ? moment(po.start_date).format('yyyy-MM-D HH:mm:ss')
+      : null,
+    end_date: po.end_date
+      ? moment(po.end_date).format('yyyy-MM-D HH:mm:ss')
+      : null,
     remark_weekly: po.remark_weekly ? po.remark_weekly : '-',
     remark_project: {
       id: po.remark_project ? po.remark_project.id : null,
@@ -104,8 +110,8 @@ export const PurchaseOrderDetailResource = (po: PurchaseOrder): any => {
       name: po.pd ? po.pd.name : '-',
     },
     actual_completion_date_vs_to_pd: po.actual_completion_date
-      ? po.actual_completion_date
-      : '-',
+      ? moment(po.actual_completion_date).format('yyyy-MM-D HH:mm:ss')
+      : null,
     ready_invoice: po.ready_invoice ? po.ready_invoice : '-',
     amount_ready_invoice: po.amount_ready_invoice
       ? Number(po.amount_ready_invoice)
@@ -114,11 +120,17 @@ export const PurchaseOrderDetailResource = (po: PurchaseOrder): any => {
       ? po.po_invoice.map((e) => {
           return {
             invoice_number: e.invoice_number,
-            date: e.invoice_date,
+            date: e.invoice_date
+              ? moment(e.invoice_date).format('yyyy-MM-D HH:mm:ss')
+              : null,
             status: e.invoice_status,
-            payment_date: e.payment_date,
+            payment_date: e.payment_date
+              ? moment(e.payment_date).format('yyyy-MM-D HH:mm:ss')
+              : null,
             supplier_tax_number: e.supplier_tax_number,
-            supplier_tax_date: e.supplier_tax_date,
+            supplier_tax_date: e.supplier_tax_date
+              ? moment(e.supplier_tax_date).format('yyyy-MM-D HH:mm:ss')
+              : null,
           };
         })
       : [],

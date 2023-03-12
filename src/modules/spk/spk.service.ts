10972ee90d76f987e5654f44bb259583334dcf45
bayunvnt <bayunoviantoo9@gmail.com>
2023-03-12 23:17:36 +0700

    feat(spk): add status filter

--- src/modules/spk/spk.service.ts
+++ src/modules/spk/spk.service.ts
@@ -343,13 +343,13 @@ export class SPKService {
 
     if (paginationOptions.start_date) {
       data.andWhere('spk.created_at >= :start_date', {
-        start_date: `%${paginationOptions.start_date}%`,
+        start_date: `${paginationOptions.start_date}`,
       });
     }
 
     if (paginationOptions.end_date) {
       data.andWhere('spk.created_at <= :end_date', {
-        end_date: `%${paginationOptions.end_date}%`,
+        end_date: `${paginationOptions.end_date}`,
       });
     }
 

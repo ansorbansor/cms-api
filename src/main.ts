553ecad33ed413b9a1a92b0784abf924fe379203
ansorbansor <ansorbansor@gmail.com>
2025-12-16 12:24:24 +0700

    Add ss edit po

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   app.enableCors({
-    origin: 'https://smarteye.ptbiosron.my.id',
+    origin: ['https://smarteye.ptbiosron.my.id', 'https://smarteye-api.ptbiosron.com', 'https://smarteye.ptbiosron.com'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });

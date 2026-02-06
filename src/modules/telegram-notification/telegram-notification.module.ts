624fbc4539f8ad7ba5d87ba8483f0a4bb136b7b6
ansorbansor <ansorbansor@gmail.com>
2026-02-06 18:29:58 +0700

    redefine PO style

--- src/modules/telegram-notification/telegram-notification.module.ts
+++ src/modules/telegram-notification/telegram-notification.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ActivityLog } from 'src/entities/activity-log.entity';
 import { TelegramNotificationService } from './telegram-notification.service';
 import { ActivityLogModule } from '../activity-log/activity-log.module';
 
 @Module({
     imports: [
         ScheduleModule.forRoot(),
+        TypeOrmModule.forFeature([ActivityLog]),
         ActivityLogModule,
     ],
     providers: [TelegramNotificationService],
